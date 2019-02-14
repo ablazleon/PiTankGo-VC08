f32da65ec0bd9e5121d1346326d9bb0645500b40
Adrián <adrian.blazquez.leon@alumnos.upm.es>
2019-02-14 09:21:40 +0100

    Ordenados los flags ends y notaTimeout

--- piTankGoLib.h
+++ piTankGoLib.h
@@ -70,8 +70,8 @@
 #define FLAG_START_DISPARO 		0x01
 #define FLAG_START_IMPACTO 		0x02
 #define FLAG_PLAYER_STOP 		0x04
-#define FLAG_PLAYER_END			0x16
-#define FLAG_NOTA_TIMEOUT		0x32
+#define FLAG_PLAYER_END			0x08
+#define FLAG_NOTA_TIMEOUT		0x16
 
 enum interruption_sources {
 	TARGET_ISR,
